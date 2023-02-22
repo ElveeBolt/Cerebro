49ad887376520b52349ab807a0f119279f6f219e
Elvee Bolt <boltelvee@gmail.com>
2023-02-22 12:58:56 +0200

    Fix update statistics

--- cerebro/user/views.py
+++ cerebro/user/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.hashers import check_password
-from django.contrib.auth.models import User, auth
-from django.contrib.auth.password_validation import validate_password
-from django.core.exceptions import ValidationError
-from .models import History, Login
+from django.contrib.auth.models import auth
+from .models import History, Login, User
 from statistic.models import Statistics
 from api import api
 from django.conf import settings
