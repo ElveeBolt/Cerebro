a2ca5238ed1a5a0f55846e419c30d46388fb4a7e
Elvee Bolt <boltelvee@gmail.com>
2023-03-07 08:49:06 +0200

    Added inlines to Database and Posts

--- cerebro/databases/admin.py
+++ cerebro/databases/admin.py
@@ -3,15 +3,28 @@ from .models import Database, Post, PostImage
 
 
 # Register your models here.
+class PostInline(admin.TabularInline):
+    model = Post
+    extra = 0
+
+
 @admin.register(Database)
 class Database(admin.ModelAdmin):
     list_display = ('title', 'index', 'actuality', 'category', 'country')
     list_filter = ('actuality', 'category', 'country')
+    inlines = (PostInline,)
+
+
+class PostImageInline(admin.StackedInline):
+    model = PostImage
+    extra = 0
+    readonly_fields = ('image_preview',)
 
 
 @admin.register(Post)
 class Post(admin.ModelAdmin):
     list_display = ('title', 'database')
+    inlines = (PostImageInline,)
 
 
 @admin.register(PostImage)
